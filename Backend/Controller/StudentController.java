2d81e7abea319be6fc9660ab3c3fa3a6da47ab4b
rjsavitha1510 <rjsavitha1510@gmail.com>
2025-09-17 22:10:32 +0530

    Update StudentController.java

--- Backend/Controller/StudentController.java
+++ Backend/Controller/StudentController.java
@@ -1,8 +1,8 @@
 package com.studentapi.student.portal.management.Controller;
 
-import com.studentapi.student.portal.management.DTO.MarkDTO;
-import com.studentapi.student.portal.management.Entity.MarkEntity;
-import com.studentapi.student.portal.management.Service.MarkService;
+import com.studentapi.student.portal.management.DTO.StudentDTO;
+import com.studentapi.student.portal.management.Entity.StudentEntity;
+import com.studentapi.student.portal.management.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,29 +10,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @CrossOrigin
 @RestController
-public class MarkController {
+public class StudentController {
     @Autowired
-    private MarkService msd;
-    //post
-    @PostMapping("/postmarks")
-    public MarkEntity showmark(@RequestBody  MarkDTO ml)
+    private StudentService ss;
+    @PostMapping("/student-register")
+    public StudentEntity RegisterApi(@RequestBody StudentDTO sts)
     {
-        System.out.println("name"+ml.getName());
-        return msd.postdata(ml);
+        return ss.Registerlogic(sts);
     }
-    //get
-    @GetMapping("/getmarks/{reg}")
-    public List<MarkDTO> getstudentmark(@PathVariable(name="reg") Long reg)
+
+    @PostMapping("/loginpage")
+    public ResponseEntity<String> loginapi(@RequestBody StudentDTO sf)
     {
-        return msd.getmark(reg);
-    }
-    //update
-    @PutMapping("/putmarks")
-    public ResponseEntity<String> updatedmark(@RequestBody MarkDTO mp)
-    {
-        String res=msd.Updated(mp);
-        if(res.equals("Data updated successfully"))
+        if(ss.Loginlogic(sf.getReg(),sf.getPassword()))
         {
+            return ResponseEntity.ok("Login Successfully");//
+
+        }
+        else
+        {
+            return ResponseEntity.ok("Login failed");
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<String> Updateapi(@RequestBody StudentDTO df)//00
+    {
+        String response=ss.Update(df);//00
+
+        if(response.equals("Data updated successfully")){
             return ResponseEntity.ok("Data updated successfully");
         }
         else {
@@ -40,21 +46,17 @@ public class MarkController {
         }
     }
 
-    @GetMapping("/all-marks")
-    public List<MarkDTO> Displayall(){
-        return msd.getall();
+    @GetMapping("/displaydata/{reg}")
+    public List<StudentDTO> Displayapi(@PathVariable(name="reg") Long reg)//0
+    {
+        return ss.DisplayUsingRegLogic(reg);//0
     }
 
-  @DeleteMapping(path="/delete/{reg}")
-    public ResponseEntity<String> Markdelete(@PathVariable Long reg)
+  @DeleteMapping("/student-delete/{reg}")
+    public String deletereg(@PathVariable(name = "reg") long reg)
   {
-      String response=msd.Deletelogic(reg);
-      if(response.equals("deleted successfully"))
-      {
-          return ResponseEntity.ok("deleted successfully");
-      }
-      else {
-          return ResponseEntity.ok("delete failed");
-      }
+      return ss.deletebyid(reg);
   }
+
+
 }
